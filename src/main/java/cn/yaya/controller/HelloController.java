6e82408a805512e9f24b0564f96b4b8146f5e449
moesquirrel <382005138@qq.com>
2019-08-25 21:31:13 +0800

    提交

--- src/main/java/cn/yaya/controller/HelloController.java
+++ src/main/java/cn/yaya/controller/HelloController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public  String hello(){
+
         return "hello controller";
     }
 }
