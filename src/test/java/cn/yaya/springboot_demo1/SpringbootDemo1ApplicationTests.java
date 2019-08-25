505c078a6a33ee26f29bd2dd85160a146801f2ff
moesquirrel <382005138@qq.com>
2019-08-25 19:44:56 +0800

    提交

---
+++ src/test/java/cn/yaya/springboot_demo1/SpringbootDemo1ApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.yaya.springboot_demo1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootDemo1ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
